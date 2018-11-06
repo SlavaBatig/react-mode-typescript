fe1479ee16f0c569a0ba180609fa793a5520a415
Vyacheslav <vyacheslav@code-care.pro>
2018-11-06 14:29:11 +0200

    added user deleting function

--- server/services/crud.ts
+++ server/services/crud.ts
@@ -7,11 +7,15 @@ export class CrudService {
         try {
             const user = await User.findById(req.params.id).lean().exec();
             res.json({
-                user: user? user : null
+                user: user? user : null,
+                result: 'OK'
             });
         } catch (err) {
             console.log(err);
-            res.json({user: null});
+            res.json({
+                message: 'Error while searching for user',
+                result: 'FAIL'
+            });
         }
     }
 
@@ -20,19 +24,49 @@ export class CrudService {
             const { body } = req;
             const user = await User.findByIdAndUpdate(req.params.id, body, {new: true}).lean().exec();
             res.json({
+                result: 'OK',
+                message: 'User has been updated',
                 user: user? user : null
             });
         } catch (err) {
             console.log(err);
-            res.json({message: 'error'});
+            res.json({
+                message: 'Error while updating user',
+                result: 'FAIL'
+            });
+        }
+    }
+
+    public async deleteUser(req: Request, res: Response) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id).lean().exec();
+            res.json({
+                message: `${user.name} has been successfully deleted`,
+                result: 'OK'
+            });
+        } catch (err) {
+            console.log(err);
+            res.json({
+                message: 'Error while deleting user',
+                result: 'FAIL'
+            });
         }
     }
 
     public async getUsers(req: Request, res: Response) {
         console.log(new Date(), 'in /users');
-        const users = await User.find({}).lean().exec();
-        res.json({
-            users: users.length > 0 ? users : []
-        });
+        try {
+            const users = await User.find({}).lean().exec();
+            res.json({
+                users: users.length > 0 ? users : [],
+                result: 'OK'
+            });
+        } catch (err) {
+            console.log(err);
+            res.json({
+                message: 'Erorr while searching for users',
+                result: 'FAIL'
+            });
+        }
     }
 }
